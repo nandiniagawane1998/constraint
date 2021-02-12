af27ee7669204e1bbb33a9572559560dacfb341d
NandiniAgawane <nandiniagawane1998@gmail.com>
2021-02-13 00:59:56 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var clog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,6 +20,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    clog = new Log(280,120,80,PI/2);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,7 +38,17 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+   
+    var options = {
+        bodyA : bird.body,
+        bodyB : clog.body,
+        stiffness : 0.04,
+        length : 10
+    }
+    
+    var chain = Constraint.create(options);
+    World.add(world,chain);
+   
 }
 
 function draw(){
@@ -61,4 +74,9 @@ function draw(){
 
     bird.display();
     platform.display();
+    clog.display();
+    
+    strokeWeight(3);
+    line(bird.body.position.x,bird.body.position.y,clog.body.position.x,clog.body.position.y);
+
 }
